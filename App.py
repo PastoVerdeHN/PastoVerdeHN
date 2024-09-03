ad9f9cbb2fd816ab64c9790e962f99b18c901358
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-03 11:19:28 -0600

    Update App.py

--- App.py
+++ App.py
@@ -204,13 +204,20 @@ def main():
         # Display the current page
         menu_items[st.session_state.current_page]()
 
-        if st.sidebar.button("🚪 Log Out,📄 Terms and Conditions"):
+        if st.sidebar.button("🚪 Log Out"):
             st.session_state.user = None
             st.success("Logged out successfully.")
             st.experimental_rerun()
 
         st.sidebar.markdown("---")
         st.sidebar.markdown("[Terms and Conditions](/Terms_and_Conditions)")
+    else:
+        st.write("Please log in to access the full features of the app")
+        st.sidebar.markdown("---")
+        st.sidebar.markdown("[Terms and Conditions](/Terms_and_Conditions)")
+
+if __name__ == "__main__":
+    main()
     
     else:
         st.write("Please log in to access the full features of the app")
