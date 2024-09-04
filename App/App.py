613044a4932801b5ded859efd362e848a04b135d
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-03 22:03:26 -0600

    Update App.py

--- App/App.py
+++ App/App.py
@@ -157,7 +157,7 @@ def main():
         st.write("Please log in to access Pasto Verde services")
 
     st.sidebar.markdown("---")
-    st.sidebar.markdown("[Terms and Conditions](/Terms_and_Conditions)")
+    st.sidebar.markdown("[Terms and Conditions]"(App/Pages/Terms_and_Conditions)")
 
 def home_page():
     st.write(f"Welcome to Pasto Verde, {st.session_state.user.name}! 🌿")
