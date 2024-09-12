53c3c7b2413186e35ade073b9e4eb700005fd135
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-12 14:23:54 -0600

    Update Productos.py

--- App/pages/Productos.py
+++ App/pages/Productos.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
 def app():
-    st.title("Productos")
+    
+    st.set_page_config(
+    page_title="Productos",
+    page_icon="🌿",
+    layout="wide"
+)
     
     # Replace 'https://example.com' with your actual website URL
     website_url = "https://pastoverde.durablesites.com/productos?pt=NjZjZmZiNmQzMzBjMWZmZWVjOWY4OWRhOjE3MjQ5MTgwODYuOTQ1OnByZXZpZXc="
