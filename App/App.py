d09ba68ef2ec1f14f0515315bf0c1965883b375b
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-12 21:49:15 -0600

    Update App.py

--- App/App.py
+++ App/App.py
@@ -165,7 +165,7 @@ def place_order():
             ]
         },
         "Suscripción Semestral": {
-            "price": L.899.00,
+            "price": 899.00,
             "features": [
                 "Entrega cada dos semanas",
                 "Envío gratis",
