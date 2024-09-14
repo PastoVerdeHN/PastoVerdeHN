4e99887ae2617302ea6a199525e27276fda94453
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-14 04:19:23 -0600

    Update App.py

--- App/App.py
+++ App/App.py
@@ -11,9 +11,10 @@ import os
 from dotenv import load_dotenv
 from auth0_component import login_button
 from branca.element import Template, MacroElement
+from models import User, Product, Order, setup_database
 from geopy.geocoders import Nominatim
 import time
-import models
+from models import Order
 
 # Streamlit page configuration
 st.set_page_config(
