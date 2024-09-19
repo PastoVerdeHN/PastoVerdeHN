dd4ace7e9a7cb449bc6f52e518b5848fcb6b421c
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-18 22:19:47 -0600

    Update Formulario.py

--- App/pages/Formulario.py
+++ App/pages/Formulario.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 def app():
   # Configure the Streamlit page
   st.set_page_config(
@@ -5,7 +7,7 @@ def app():
       page_icon="🌿",
       layout="wide"
   )
-  
+
   # Load and display the sidebar image
   image_url = "https://raw.githubusercontent.com/PastoVerdeHN/PastoVerdeHN/main/STREAMLIT%20PAGE%20ICON.png"
   st.sidebar.image(image_url, use_column_width=True, caption="La Naturaleza A Los Pies De Tus Mascota")
