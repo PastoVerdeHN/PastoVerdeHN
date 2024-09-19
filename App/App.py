bc31f764eb5b95eda3781daa66c8a9351649686e
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-18 22:47:08 -0600

    Update App.py

--- App/App.py
+++ App/App.py
@@ -56,7 +56,7 @@ def auth0_authentication():
   if 'auth_status' not in st.session_state:
       st.session_state.auth_status = None
   if st.session_state.user is None:
-      auth_choice = st.sidebar.radio("Choose action", ["🔑 Entrar"])
+      auth_choice = st.sidebar.radio("Elige acción", ["🔑 Entrar"])
       
       if auth_choice == "🔑 Entrar":
           try:
