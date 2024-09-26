1e45bec3301dfda1d4ab1d75adf4cc16fe893558
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-26 14:56:14 -0600

    Update App.py

--- App/App.py
+++ App/App.py
@@ -175,41 +175,7 @@ def main():
     
 
 
-def display_user_orders():
-  st.subheader("📦 Mis Órdenes")
-  
-  session = Session()
-  orders = session.query(Order).filter_by(user_id=st.session_state.user.id).all()
-  
-  # Define order status mapping
-  status_mapping = {
-      OrderStatus.pending: ("Pago Pendiente", 0),
-      OrderStatus.confirmed: ("Orden Confirmada", 33),
-      OrderStatus.shipped: ("Orden Enviada", 66),
-      OrderStatus.delivered: ("Orden Entregada", 100)
-  }
-  
-  for order in orders:
-      with st.expander(f"Order ID: {order.id} - Status: {order.status.value}"):
-          st.write(f"**Fecha de entrega:** {order.date}")
-          st.write(f"**Dirección de entrega:** {order.delivery_address}")
-          st.write(f"**Precio total:** L. {order.total_price:.2f}")
-          
-          if order.product_id:
-              product = session.query(Product).filter_by(id=order.product_id).first()
-              if product:
-                  st.write(f"**Producto:** {product.name}")
 
-          # Display progress bar based on order status
-          if order.status in status_mapping:
-              status_label, progress_value = status_mapping[order.status]
-              st.write(f"**Estado del pedido:** {status_label}")
-              progress_bar = st.progress(progress_value)
-          else:
-              st.write("**Estado del pedido:** Desconocido")
-              st.progress(0)  # Default to 0 if status is unknown
-
-  session.close()
 def user_manual():
   st.subheader("📖 Manual de Usuario")
   st.write("""
