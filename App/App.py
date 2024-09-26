9bff644a5d0f9037b98a9238b4547b422495cc54
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-26 09:12:45 -0600

    Update App.py

--- App/App.py
+++ App/App.py
@@ -293,7 +293,19 @@ def place_order():
   delivery_date = st.date_input("Fecha de entrega", value=datetime.today())
 
   # Delivery Time Frame Selection
-  delivery_time_frame = st.radio("Selecciona un horario de entrega:", ("AM (7am - 12pm)", "PM (NO DISPONIBLE)"))
+options = [
+    "AM (7am - 12pm)",
+    "PM (NO DISPONIBLE)"
+]
+
+delivery_time_frame = st.radio(
+    "Selecciona un horario de entrega:",
+    options,
+    index=0,
+    disabled=[False, True]
+)
+
+st.write(f"Has seleccionado: {delivery_time_frame}")
 
   # Promo Code Input and Disclaimer
   promo_code = st.text_input("Código promocional (opcional)", value="")
