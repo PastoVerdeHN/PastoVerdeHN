41b875fa5f9e46b95cf6b968bf4b05a81bf2daf3
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-26 14:02:57 -0600

    Update and rename home.py to home.py

---
+++ App/modules/home.py
@@ -0,0 +1,10 @@
+   import streamlit as st
+
+   def home_page():
+       st.write(f"Bienvenido/a Pasto Verde, {st.session_state.user.name}! 🌿")
+       st.write("¡Llevando pasto fresco a tus mascotas, una caja a la vez!")
+       
+       st.subheader("Conozca Pasto Verde")
+       
+       video_url = "https://github.com/PastoVerdeHN/PastoVerdeHN/raw/main/PASTO%20VERDE%20AD%20FINAL.mp4"
+       st.video(video_url)
