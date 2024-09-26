0cce2722697871f3dc6e79902ff2ee7487e60883
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-26 14:41:05 -0600

    Update App.py

--- App/App.py
+++ App/App.py
@@ -21,6 +21,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 import requests
 from modules.home import home_page
+from modules.home import place_order
 
 print("Importing home module")
 
