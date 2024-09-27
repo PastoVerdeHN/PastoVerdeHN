d73c053a24e912cf188513346b98d5fbadd2fba8
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-26 22:37:31 -0600

    Update __init__.py

--- App/modules/__init__.py
+++ App/modules/__init__.py
@@ -3,3 +3,4 @@ from .order import place_order
 from .user_orders import display_user_orders
 from .auth import auth0_authentication
 from .email import send_welcome_email
+from .map import display_map
