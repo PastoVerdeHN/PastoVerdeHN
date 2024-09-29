9f3d8ee1595f9f1c716afcce5b7cd77b1c8060aa
PastoVerdeHN <pastoverdehn@gmail.com>
2024-09-29 01:25:22 -0600

    Create AsistenteVirtual.py

---
+++ App/pages/AsistenteVirtual.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Retrieve the iframe URL from the secrets file
+iframe_url = st.secrets["iframe_url"]
+
+# Create the iframe HTML using the secret URL
+iframe_html = f"""
+<iframe title="embed" src="{iframe_url}" width="100%" height="500px" style="background:white"></iframe>
+"""
+
+# Render the iframe using Streamlit's components.html function
+components.html(iframe_html, height=500)
