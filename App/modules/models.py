af2dbdff3cae3e82ecfde390c895fcd8627abd33
PastoVerdeHN <pastoverdehn@gmail.com>
2024-10-02 22:01:05 -0600

    Update models.py

--- App/modules/models.py
+++ App/modules/models.py
@@ -93,8 +93,6 @@ class PaymentTransaction(Base):
     payment_method = Column(String)
     order = relationship("Order")
 
-
-    
 def setup_database(database_url):
     engine = create_engine(database_url, echo=True)
     
