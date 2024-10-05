1332e0c9f9128dbaf867eba8a1af389aa4976cb6
PastoVerdeHN <pastoverdehn@gmail.com>
2024-10-04 23:52:43 -0600

    Update Preguntas Frecuentes.py

--- App/pages/Preguntas Frecuentes.py
+++ App/pages/Preguntas Frecuentes.py
@@ -37,7 +37,7 @@ def app():
 
   # Load and display the sidebar image
   image_url = "https://raw.githubusercontent.com/PastoVerdeHN/PastoVerdeHN/main/STREAMLIT%20PAGE%20ICON.png"
-  st.sidebar.image(image_url, use_column_width=True, caption="La Naturaleza A Los Pies De Tus Mascota")
+  st.sidebar.image(image_url, use_column_width=True, caption="© La Naturaleza A Los Pies De Tus Mascota")
 
   st.title("Preguntas Frecuentes")
 
