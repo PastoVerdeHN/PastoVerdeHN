537fbcc6859180614b88435b8005b40802fcbb2e
PastoVerdeHN <pastoverdehn@gmail.com>
2025-01-04 14:09:18 -0600

    Update 8_📞LlamadaVirtual.py

--- App/pages/8_📞LlamadaVirtual.py
+++ App/pages/8_📞LlamadaVirtual.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 # Streamlit app layout
 st.title("Llamada Virtual - Atención al Cliente")
@@ -10,8 +11,8 @@ st.markdown("""
     No dudes en hacer cualquier pregunta relacionada con nuestros servicios.
 """)
 
-# Embedding Eleven Labs Virtual Call widget using markdown with unsafe_allow_html
-st.markdown("""
+# Embedding Eleven Labs Virtual Call widget using st.components.v1.html
+components.html("""
     <elevenlabs-convai agent-id="m5tRR9UgIevQCBy90gvh"></elevenlabs-convai>
     <script src="https://elevenlabs.io/convai-widget/index.js" async type="text/javascript"></script>
-""", unsafe_allow_html=True)
+""", height=600)
