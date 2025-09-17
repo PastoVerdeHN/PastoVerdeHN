fb9a4494bc03ca29f88037a96a6affa01f4c1837
PastoVerdeHN <pastoverdehn@gmail.com>
2025-09-17 08:15:23 -0600

    Update 9_📜Términos_y_ Condiciones.py

--- App/pages/9_📜Términos_y_ Condiciones.py
+++ App/pages/9_📜Términos_y_ Condiciones.py
@@ -17,39 +17,44 @@ def main():
 def terms_and_privacy():
   st.subheader("📜 Términos de Servicio de ©Pasto Verde")
   st.write("""
-  ¡Bienvenido a los términos de servicio de ©Pasto Verde! Al utilizar nuestros servicios, usted acepta los siguientes términos y condiciones:
+    ¡Bienvenido a los términos de servicio de ©Pasto Verde! Al utilizar nuestros servicios, usted acepta los siguientes términos y condiciones:
 
-  **Dirección de Entrega**  
-  El cliente es responsable de proporcionar una dirección de entrega correcta y accesible para recibir las alfombras de césped natural. ©Pasto Verde no se hace responsable por cualquier daño o deterioro ocurrido a la alfombra de césped después de la entrega exitosa en la dirección proporcionada.
+    **Dirección de Entrega**
+    El cliente es responsable de proporcionar una dirección de entrega correcta y accesible para recibir las alfombras de césped natural. ©Pasto Verde no se hace responsable por cualquier daño o deterioro ocurrido a la alfombra de césped después de la entrega exitosa en la dirección proporcionada.
 
-  **Responsabilidad Post-Entrega**  
-  ©Pasto Verde no se responsabiliza por el estado del césped natural una vez que ha sido entregado en la dirección indicada por el cliente. Esto incluye daños causados por factores ambientales, mascotas u otros elementos fuera de nuestro control.
+    **Responsabilidad Post-Entrega**
+    ©Pasto Verde no se responsabiliza por el estado del césped natural una vez que ha sido entregado en la dirección indicada por el cliente. Esto incluye daños causados por factores ambientales, mascotas u otros elementos fuera de nuestro control.
 
-  **Costos de Envío**  
-  El costo de envío varía según el plan de suscripción o compra única seleccionada. Los cargos aplicables se detallarán al finalizar la compra y pueden estar sujetos a cambios según la ubicación y la modalidad de entrega seleccionada.
+    **Bases de Alfombras**
+    Las bases de las alfombras (de cartón, de madera normal o con drenaje) se venden por separado y pueden tener costos adicionales dependiendo de las opciones de personalización elegidas por el cliente.
 
-  **Bases de Alfombras**  
-  Las bases de las alfombras (de cartón, de madera normal o con drenaje) se venden por separado y pueden tener costos adicionales dependiendo de las opciones de personalización elegidas por el cliente.
+    **Cargos de Envío**
+    El costo de envío varía según el plan de suscripción o compra única seleccionada. Los cargos aplicables se detallarán al finalizar la compra y pueden estar sujetos a cambios según la ubicación y la modalidad de entrega seleccionada.
 
-  **Cancelación de Suscripción**  
-  El cliente puede cancelar su suscripción en cualquier momento. Sin embargo, no se realizarán reembolsos por el período pagado ya transcurrido. Se recomienda a los clientes gestionar la cancelación con antelación para evitar cargos no deseados.
+    **Política de Cancelación y Devoluciones**
+    **1. Cancelación de Suscripción:** El cliente puede cancelar su suscripción en cualquier momento. Sin embargo, no se realizarán reembolsos por el período pagado ya transcurrido. Si un pedido ya ha sido pagado y enviado, o si se han incurrido en costos de inventario o preparación, se aplicará una tarifa de cancelación y reabastecimiento que se deducirá de cualquier posible reembolso.
 
-  **Modificaciones de Precios y Condiciones**  
-  ©Pasto Verde se reserva el derecho de modificar los precios y las condiciones del servicio en cualquier momento. Se notificará al cliente de cualquier cambio con antelación adecuada para que pueda tomar decisiones informadas sobre la continuidad de su suscripción o compra.
+    **2. Precio de Reembolso:** Ofrecemos precios especiales con descuento a través de nuestros planes de suscripción. **Cualquier reembolso o cálculo de tarifas por cancelación se basará en el precio minorista (precio completo) de los productos y no en los precios promocionales o de suscripción.**
 
-  **Reemplazo de Césped Natural**  
-  Para garantizar la salud y bienestar de la mascota, se recomienda reemplazar el césped natural según el plan de suscripción elegido. El cliente es responsable de mantener el césped en condiciones adecuadas hasta su reemplazo.
+    **3. Productos Personalizados:** Todos los pedidos de productos personalizados, como la caja de madera, no son elegibles para reembolsos una vez que el pedido ha sido procesado. El cliente es responsable del costo total del producto.
 
-  **Propiedad Intelectual**  
-Todo el contenido de este sitio web y del código de la aplicación, incluyendo, pero no limitado a, imágenes, videos, gráficos, diseño, disposición, texto y cualquier material generado por LLMs, es propiedad exclusiva de ©Pasto Verde HN. Todos los derechos están reservados. Cualquier uso, reproducción o distribución no autorizada de estos materiales, así como cualquier intento de copiar o reproducir el código de la aplicación, está estrictamente prohibido y puede dar lugar a acciones legales.
+    **4. Devoluciones:** No se aceptan devoluciones de productos perecederos, como el césped natural. En el caso de productos no perecederos, si se acepta una devolución, el cliente es responsable de todos los costos de envío. Los productos deben ser devueltos en su condición original para que el proceso de reembolso sea considerado.
 
+    **Reemplazo de Césped Natural**
+    Para garantizar la salud y bienestar de la mascota, se recomienda reemplazar el césped natural según el plan de suscripción elegido. El cliente es responsable de mantener el césped en condiciones adecuadas hasta su reemplazo.
 
+    **Modificaciones de Precios y Condiciones**
+    ©Pasto Verde se reserva el derecho de modificar los precios y las condiciones del servicio en cualquier momento. Se notificará al cliente de cualquier cambio con antelación adecuada para que pueda tomar decisiones informadas sobre la continuidad de su suscripción o compra.
 
-  **Cumplimiento Legal**  
-  ©Pasto Verde se adhiere a todas las leyes y regulaciones aplicables en Honduras, incluyendo aquellas relacionadas con la protección del consumidor, privacidad y publicidad.
+    **Propiedad Intelectual**
+    Todo el contenido de este sitio web y del código de la aplicación, incluyendo, pero no limitado a, imágenes, videos, gráficos, diseño, disposición, texto y cualquier material generado por LLMs, es propiedad exclusiva de ©Pasto Verde HN. Todos los derechos están reservados. Cualquier uso, reproducción o distribución no autorizada de estos materiales, así como cualquier intento de copiar o reproducir el código de la aplicación, está estrictamente prohibido y puede dar lugar a acciones legales.
 
-  Al utilizar nuestros servicios, usted acepta estos términos y condiciones. Si tiene alguna pregunta o inquietud, no dude en ponerse en contacto con nosotros.
-  """)
+    **Cumplimiento Legal**
+    ©Pasto Verde se adhiere a todas las leyes y regulaciones aplicables en Honduras, incluyendo aquellas relacionadas con la protección del consumidor, privacidad y publicidad.
+
+    Al utilizar nuestros servicios, usted acepta estos términos y condiciones. Si tiene alguna pregunta o inquietud, no dude en ponerse en contacto con nosotros.
+    """)
+
 
   st.subheader("🔒 Política de Privacidad de ©Pasto Verde")
   st.write("""
